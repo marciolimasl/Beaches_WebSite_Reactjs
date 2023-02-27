29d63bb16c7e1d397cf2c46d0598cf4bdd55f793
marciolimasl <marciolimasl2002@gmail.com>
2023-02-27 13:25:23 -0300

    Search/Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Destinations from "./components/destinations";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
+import Search from "./components/Search";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Destinations />
+      <Search />
     </div>
   );
 }
